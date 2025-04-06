1f57da9022d20080afdb077bd7130b94eace4ccb
suraj7299 <surajbrnwal2424@gmail.com>
2025-04-06 11:56:22 +0530

    add initial environment configuration and lodash module exports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "./components/ui/button";
 
 
 function App() {
@@ -6,6 +8,18 @@ function App() {
   return (
     <>
       <h1 className="text-red-500 text-5xl">Hello</h1>
+      <header>
+      <SignedOut>
+        <SignInButton> 
+          <Button>
+            Sign In
+          </Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
     </>
   )
 }
