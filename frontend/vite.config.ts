aa16a1b1c819dde1a1fb1530665a5085e4beec07
suraj7299 <surajbrnwal2424@gmail.com>
2025-04-19 19:49:08 +0530

    chore: update dependencies and configuration for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,15 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    chunkSizeWarningLimit: 1500 
-  }
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	server: {
+		port: 3000,
+	},
+});
